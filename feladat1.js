fd2b29f900944649631ddaae850e071e5159d591
foldhazicsaba <92531570+foldhazicsaba@users.noreply.github.com>
2022-02-19 23:49:37 +0100

    Add files via upload

---
+++ feladat1.js
@@ -0,0 +1,10 @@
+
+    function kicsinyit(){
+        uj = document.Teszt.bevitel.value.toLowerCase();
+        document.Teszt.bevitel.value = uj;
+       }
+       function nagyit(){
+        uj = document.Teszt.bevitel.value.toUpperCase();
+        document.Teszt.bevitel.value = uj;
+       
+}
